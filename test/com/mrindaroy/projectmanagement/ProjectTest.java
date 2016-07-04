fbf6fc2f05c1833985b11ef11cd17de07e6bb4da
Martin Rindaroy <mri@icd.no>
2016-07-04 23:36:09 +0200

    Removed the tests for single members and tasks

--- test/com/mrindaroy/projectmanagement/ProjectTest.java
+++ test/com/mrindaroy/projectmanagement/ProjectTest.java
@@ -9,95 +9,82 @@ import static org.junit.Assert.assertEquals;
 public class ProjectTest {
 
   private Project project;
-  private ProjectTask task;
-  private ProjectMember member;
+  private ProjectTask firstTask;
+  private ProjectMember firstMember;
+  private ProjectTask secondTask;
+  private ProjectMember secondMember;
 
   @Before
   public void setUp() throws Exception {
     project = new Project();
-    task = new ProjectTask();
-    member = new ProjectMember();
+    firstTask = new ProjectTask();
+    firstMember = new ProjectMember();
+    secondTask = new ProjectTask();
+    secondMember = new ProjectMember();
 
-    task.setId("taskID");
-    member.setId("memberID");
-    project.addTask(task);
-    project.addMember(member);
-    project.addMemberToTask(member.getId(), task.getId());
+    firstTask.setId("firstTaskID");
+    firstMember.setId("firstMemberID");
+    project.addTask(firstTask);
+    project.addMember(firstMember);
+    project.addMemberToTask(firstMember.getId(), firstTask.getId());
+
+    secondTask.setId("secondTaskID");
+    secondMember.setId("secondMemberID");
+    project.addTask(secondTask);
+    project.addMember(secondMember);
+    project.addMemberToTask(secondMember.getId(), secondTask.getId());
   }
 
   @Test
-  public void getRegisteredHours() throws Exception {
-    double hoursSpent = 12.34;
-    project.registerHours(member.getId(), task.getId(), hoursSpent);
+  public void getHoursRemainingWithMultipleMembersAndTasks() throws Exception {
+    double hoursSpentOnTask1 = 12.34;
+    double hoursEstimatedForTask1 = 37.5;
+    firstTask.setHoursEstimated(hoursEstimatedForTask1);
 
-    assertEquals(hoursSpent, task.getHoursRegistered(), MIN_VALUE);
-    assertEquals(hoursSpent, project.getHoursRegistered(), MIN_VALUE);
-  }
+    double hoursSpentOnTask2 = 12.34;
+    double hoursEstimatedForTask2 = 37.5;
+    secondTask.setHoursEstimated(hoursEstimatedForTask2);
 
-  @Test
-  public void getHoursRemaining() throws Exception {
-    double hoursSpent = 12.34;
-    double hoursEstimated = 37.5;
-    task.setHoursEstimated(hoursEstimated);
+    project.registerHours(firstMember.getId(), firstTask.getId(), hoursSpentOnTask1);
+    project.registerHours(secondMember.getId(), secondTask.getId(), hoursSpentOnTask2);
 
-    project.registerHours(member.getId(), task.getId(), hoursSpent);
-
-    double hoursRemaining = hoursEstimated - hoursSpent;
-    assertEquals(hoursRemaining, task.getHoursRemaining(), MIN_VALUE);
-    assertEquals(hoursRemaining, project.getHoursRemaining(), MIN_VALUE);
+    double hoursRemainingForTask1 = hoursEstimatedForTask1 - hoursSpentOnTask1;
+    double hoursRemainingForTask2 = hoursEstimatedForTask2 - hoursSpentOnTask2;
+    double hoursRemainingForProject = hoursRemainingForTask1 + hoursRemainingForTask2;
+    assertEquals(hoursRemainingForTask1, firstTask.getHoursRemaining(), MIN_VALUE);
+    assertEquals(hoursRemainingForTask2, secondTask.getHoursRemaining(), MIN_VALUE);
+    assertEquals(hoursRemainingForProject, project.getHoursRemaining(), MIN_VALUE);
   }
 
   @Test
   public void getRegisteredHoursWithMultipleMembersAndTasks() throws Exception {
-    ProjectMember memberTwo = new ProjectMember();
-    ProjectTask taskTwo = new ProjectTask();
-    project.addTask(taskTwo);
-    project.addMember(memberTwo);
-    project.addMemberToTask(memberTwo.getId(), taskTwo.getId());
-
     double hoursSpentOnTask1 = 12.34;
-    project.registerHours(member.getId(), task.getId(), hoursSpentOnTask1);
     double hoursSpentOnTask2 = 10.34;
-    project.registerHours(memberTwo.getId(), taskTwo.getId(), hoursSpentOnTask2);
+    project.registerHours(firstMember.getId(), firstTask.getId(), hoursSpentOnTask1);
+    project.registerHours(secondMember.getId(), secondTask.getId(), hoursSpentOnTask2);
 
-    assertEquals(hoursSpentOnTask1, task.getHoursRegistered(), MIN_VALUE);
-    assertEquals(hoursSpentOnTask2, taskTwo.getHoursRegistered(), MIN_VALUE);
+    assertEquals(hoursSpentOnTask1, firstTask.getHoursRegistered(), MIN_VALUE);
+    assertEquals(hoursSpentOnTask2, secondTask.getHoursRegistered(), MIN_VALUE);
     assertEquals(hoursSpentOnTask1 + hoursSpentOnTask2, project.getHoursRegistered(), MIN_VALUE);
   }
 
   @Test
-  public void getSpending() throws Exception {
-    double hoursSpent = 12.34;
-    project.registerHours(member.getId(), task.getId(), hoursSpent);
-    double hourlyRate = 100.0;
-    member.setHourlyRate(hourlyRate);
-
-    assertEquals(hoursSpent * hourlyRate, project.getSpending(), MIN_VALUE);
-  }
-
-  @Test
   public void getSpendingWithMultipleMembersAndTasks() throws Exception {
-    ProjectMember memberTwo = new ProjectMember();
-    ProjectTask taskTwo = new ProjectTask();
-    project.addTask(taskTwo);
-    project.addMember(memberTwo);
-    project.addMemberToTask(memberTwo.getId(), taskTwo.getId());
-
     double hoursSpentOnTask1 = 12.34;
-    project.registerHours(member.getId(), task.getId(), hoursSpentOnTask1);
-    double hourlyRate1 = 100.0;
-    member.setHourlyRate(hourlyRate1);
+    double hourlyRateForMember1 = 100.0;
+    project.registerHours(firstMember.getId(), firstTask.getId(), hoursSpentOnTask1);
+    firstMember.setHourlyRate(hourlyRateForMember1);
 
     double hoursSpentOnTask2 = 10.34;
-    project.registerHours(memberTwo.getId(), taskTwo.getId(), hoursSpentOnTask2);
-    double hourlyRate2 = 200.0;
-    memberTwo.setHourlyRate(hourlyRate2);
+    double hourlyRateForMember2 = 200.0;
+    project.registerHours(secondMember.getId(), secondTask.getId(), hoursSpentOnTask2);
+    secondMember.setHourlyRate(hourlyRateForMember2);
 
-    double spendingTask1 = hoursSpentOnTask1 * hourlyRate1;
-    double spendingTask2 = hoursSpentOnTask2 * hourlyRate2;
-    double totalSpending = spendingTask1 + spendingTask2;
-    assertEquals(spendingTask1, project.getTask(task.getId()).getSpending(), MIN_VALUE);
-    assertEquals(spendingTask2, project.getTask(taskTwo.getId()).getSpending(), MIN_VALUE);
-    assertEquals(totalSpending, project.getSpending(), MIN_VALUE);
+    double spendingTask1 = hoursSpentOnTask1 * hourlyRateForMember1;
+    double spendingTask2 = hoursSpentOnTask2 * hourlyRateForMember2;
+    double totalSpendingForProject = spendingTask1 + spendingTask2;
+    assertEquals(spendingTask1, project.getTask(firstTask.getId()).getSpending(), MIN_VALUE);
+    assertEquals(spendingTask2, project.getTask(secondTask.getId()).getSpending(), MIN_VALUE);
+    assertEquals(totalSpendingForProject, project.getSpending(), MIN_VALUE);
   }
 }
